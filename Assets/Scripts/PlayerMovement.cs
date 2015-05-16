73905a9af0e41d046d6bc0b5fcd364ea0b8bb75c
Kai <kaiphas@me.com>
2015-05-16 15:53:31 +0200

    Cube

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,45 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    
+    public float speed = 1f;
+    Vector3 movement;                   // The vector to store the direction of the player's movement.
+    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+
 	// Use this for initialization
 	void Start () {
+        playerRigidbody = GetComponent<Rigidbody>();
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+
+        // Store the input axes.
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown("Jump"))
+        {
+            Jump();
+        }
+
+        // Move the player around the scene.
+        Move(h, v);
 	}
+
+    void Move(float h, float v)
+    {
+        // Set the movement vector based on the axis input.
+        movement.Set(h, 0f, v);
+
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Jump()
+    {
+        
+    }
 }
