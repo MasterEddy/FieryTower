3812e38158dd0e9ecd60fc857729eeb0390b650c
zhad3 <dragoonzombie@gmail.com>
2015-05-17 14:14:22 +0200

    SCORES

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public GameObject camera;
+    Text score_component;
+    int init_height;
+	// Use this for initialization
+	void Start () {
+        init_height = (int)camera.transform.position.y;
+        score_component = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    score_component.text = "Score: "+(float)((int)((camera.transform.position.y - init_height)*100))/100;
+	}
+}
