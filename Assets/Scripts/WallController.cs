d53399a7a577db3ac97671b25421d64dc9007919
zhad3 <dragoonzombie@gmail.com>
2015-05-17 11:15:38 +0200

    Base WallController

--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -6,29 +6,36 @@ public class WallController : MonoBehaviour {
     public GameObject[] walls_bottom;
     public GameObject[] walls_top;
     public GameObject camera;
-    public Vector3 moveUp;
-    public float SPEED = 0.1f;
+    Camera camera_component;
 	// Use this for initialization
 	void Start () {
-        moveUp = new Vector3(0f, 1f, 0f)*SPEED;
-        walls_bottom = new GameObject[2];
-        walls_top = new GameObject[2];
+        walls_bottom = new GameObject[12];
         walls_bottom = GameObject.FindGameObjectsWithTag("Wall0");
-        walls_top = GameObject.FindGameObjectsWithTag("Wall1");
-        camera = GameObject.Find("Main Camera");
+        //camera = GameObject.Find("Main Camera");
+        camera_component = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        camera.transform.Translate(moveUp * Time.deltaTime);
-            for (int i = 0; i < walls_bottom.Length; i++)
+        
+        //float viewPos = camera_component.WorldToViewportPoint()
+        for (int i = 0; i < walls_bottom.Length; i++)
+        {
+            Vector3 target = new Vector3(camera.transform.position.x, walls_bottom[i].transform.position.y - GetObjectExtents(walls_bottom[i]), camera.transform.position.z + 1f);
+            Vector3 viewPos = camera_component.WorldToViewportPoint(target);
+            Debug.Log(viewPos);
+            //walls_bottom[i].transform.Translate(-moveUp);
+            //walls_top[i].transform.Translate(-moveUp);
+            if (walls_bottom[i].transform.position.y < camera.transform.position.y - 7f)
             {
-                //walls_bottom[i].transform.Translate(-moveUp);
-                //walls_top[i].transform.Translate(-moveUp);
-                if (walls_bottom[i].transform.position.y < camera.transform.position.y - 7f)
-                {
-                    walls_bottom[i].transform.position = new Vector3(walls_bottom[i].transform.position.x, camera.transform.position.y + 1.95f, walls_bottom[i].transform.position.z);
-                }
+                walls_bottom[i].transform.position = new Vector3(walls_bottom[i].transform.position.x, camera.transform.position.y + 1.95f, walls_bottom[i].transform.position.z);
             }
+        }
 	}
+
+    float GetObjectExtents(GameObject gObj)
+    {
+        Bounds bounds = gObj.GetComponent<Collider>().bounds;
+        return bounds.extents.y;
+    }
 }
