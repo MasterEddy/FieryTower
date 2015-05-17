a9bfeb088f856c4e0fd58bd07e6c8cf2270355a5
MasterEddy <philipp.ronge@gmail.com>
2015-05-17 11:42:13 +0200

    Camera Controller2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,21 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     public GameObject camera;
     public float SPEED = 0.1f;
     public Vector3 moveUp;
+    public Vector3 addSpeed;
+
+    float accumulator = 0.0f;
+    float waitTime = 1.0f;
 
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         moveUp = new Vector3(0f, 1f, 0f) * SPEED;
         camera = GameObject.Find("Main Camera");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        addSpeed = new Vector3(0f, 0.1f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
         camera.transform.Translate(moveUp * Time.deltaTime);
-	}
+
+        // Timer for perodic speed increase
+        accumulator += Time.deltaTime;
+        if (accumulator >= waitTime)
+        {
+            moveUp += addSpeed;
+            accumulator = 0f;
+        }
+    }
 }
