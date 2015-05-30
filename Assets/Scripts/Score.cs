256b06e70427880e4f85d4805cb1e0a03cea9fac
Kai Marten <kaiphas@me.com>
2015-05-30 19:15:49 +0200

    Main Menu added + modified Death-screen

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -8,7 +8,8 @@ public class Score : MonoBehaviour {
     public GameObject score_obj;
     public GameObject es_score;
     public GameObject es_image;
-    public GameObject es_continue;
+    public GameObject es_button1;
+    public GameObject es_button2;
     Text score_component;
     Text es_score_component;
     Text es_continue_component;
@@ -20,12 +21,15 @@ public class Score : MonoBehaviour {
         init_height = (int)camera.transform.position.y;
         score_component = score_obj.GetComponent<Text>();
         es_score_component = es_score.GetComponent<Text>();
-        es_continue_component = es_continue.GetComponent<Text>();
         es_image_component = es_image.GetComponent<Image>();
 
         es_score.SetActive(false);
         es_image.SetActive(false);
-        es_continue.SetActive(false);
+        es_button1.SetActive(false);
+        es_button2.SetActive(false);
+
+        //Disbale Mouse Cursor
+        Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
@@ -36,22 +40,34 @@ public class Score : MonoBehaviour {
 
     void ScoreScreen()
     {
+        //Disbale Mouse Cursor
+        Cursor.visible = true;
         Time.timeScale = 0;
         score_obj.SetActive(false);
         es_image.SetActive(true);
         es_score.SetActive(true);
-        es_continue.SetActive(true);
+        es_button1.SetActive(true);
+        es_button2.SetActive(true);
         es_score_component.text = "Your Score:\n" + score;
         Debug.Log("SHOW SCREEN");
     }
-    void hideScoreScreen()
+    public void hideScoreScreen()
     {
+        //Disbale Mouse Cursor
+        Cursor.visible = false;
         Time.timeScale = 1;
         score_obj.SetActive(true);
         score_component.text = "Score: 0";
         es_image.SetActive(false);
         es_score.SetActive(false);
-        es_continue.SetActive(false);
+        es_button1.SetActive(false);
+        es_button2.SetActive(false);
         Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel("MainMenu");
     }
 }
